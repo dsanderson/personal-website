9b02681493dd5fb7638d24a983c990954b47e1ef
David Anderson <david@anderson.cm>
2017-01-17 13:19:01 +0800

    adding idc slides

---
+++ kitchen/timer.js
@@ -0,0 +1,9 @@
+function calc_time() {
+  var time = (1460304000 - (Date.now())/1000.0)/3600.0;
+}
+
+function set_svg() {
+  var time = calc_time()
+  var basetime = 7*24;
+  var arc = d3.select(#)
+}
